575f53e6a760721195c21cd60032fd3906245aa2
Jahed Ahmed Ripon <73092252+jahedripon9@users.noreply.github.com>
2025-05-01 11:01:41 +0600

    update

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config.js'
 import connectDB from './config/mongodb.js'
-import connectCloudinary from './config/cloudinary.js'
 import userRouter from './routes/userRoute.js'
 import productRouter from './routes/productRoute.js'
 import cartRouter from './routes/cartRoute.js'
 import orderRouter from './routes/orderRoute.js'
+import connectCloudinary from './config/cloudinary.js'
 
 // App Config
 const app = express()
