d105b22d3b408e7a7da51b83c43636992fb3d71e
Jahed Ahmed Ripon <73092252+jahedripon9@users.noreply.github.com>
2025-09-27 20:30:42 +0600

    update dependencies

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config.js'
-import connectDB from './config/mongodb.js'
+import {connectDB }from './config/mongodb.js'
 import userRouter from './routes/userRoute.js'
 import productRouter from './routes/productRoute.js'
 import cartRouter from './routes/cartRoute.js'
